77f30d0aff680c84f30be879717129abb81ab554
Artyom Bolbas <artyombolbas@yandex.ru>
2019-12-01 19:28:23 +0300

    AB-0004 Added: 'Interface Mover'

--- src/main/java/com/bolbas/app/Runner.java
+++ src/main/java/com/bolbas/app/Runner.java
@@ -1,10 +1,14 @@
 package com.bolbas.app;
 
 import com.bolbas.app.model.Car;
+import com.bolbas.app.model.Mover;
 
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello world");
         Car a = new Car();
+
+        Mover mover = () -> "I walk";
+        System.out.println(mover.getValue());
     }
 }
